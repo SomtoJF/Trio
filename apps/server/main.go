c5545e7441b5a0456225f585a2368bf14bf83b5c
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-09-05 01:00:21 +0100

    App setup

--- apps/server/main.go
+++ apps/server/main.go
@@ -1,12 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
 
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/somtojf/trio/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
 }
 
 func main() {
-	fmt.Println(Hello("server"))
+	r := gin.Default()
+	clientAddress := os.Getenv("CLIENT_ADDRESS")
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{clientAddress}
+	config.AllowCredentials = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
+
+	r.Use(cors.New(config))
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
